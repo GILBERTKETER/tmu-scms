bbd4d60042ac25353ca28f3e6914ca13dbcbbe7d
gilbertketer <gilbertketer759@gmail.com>
2024-11-17 19:32:45 +0300

    Updates

--- app/(site)/api/api.tsx
+++ app/(site)/api/api.tsx
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const App = axios.create({
-  baseURL: "http://127.0.0.1:8000",
-  // baseURL: "https://ketercoder.pythonanywhere.com",
+  baseURL: "https://ketercoder.pythonanywhere.com",
   timeout: 10000, 
   headers: {
     "Content-Type": "application/json",
