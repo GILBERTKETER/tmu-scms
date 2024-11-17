a43f8daf01bdc84c98d44f4c252b436b4a5d6617
gilbertketer <116883167+GILBERTKETER@users.noreply.github.com>
2024-11-17 08:46:54 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,16 +3,50 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ["localhost"],
+    domains: [
+      "smartcampus-lilac.vercel.app",  // Frontend domain
+      "ketercoder.pythonanywhere.com",  // Backend domain if it serves images
+      "cdn.sanity.io",  // External image sources like sanity.io
+    ],
     remotePatterns: [
       {
         protocol: "https",
         hostname: "cdn.sanity.io",
         port: "",
       },
-      
+      {
+        protocol: "https",
+        hostname: "smartcampus-lilac.vercel.app", // Frontend
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "ketercoder.pythonanywhere.com", // Backend
+        port: "",
+      },
     ],
   },
 };
 
 module.exports = nextConfig;
+
+
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   swcMinify: true,
+//   images: {
+//     domains: ["localhost"],
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: "cdn.sanity.io",
+//         port: "",
+//       },
+      
+//     ],
+//   },
+// };
+
+// module.exports = nextConfig;
